f698f68f7140e472de0ed9d54ec0320a40592ede
IqraHafeez-12 <hiqra047@gmail.com>
2026-06-04 09:17:22 +0500

    Initial Student Program

---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student {
+
+    public static void main(String[] args) {
+        
+        int studentID = 101;
+        String studentName = "Ali";
+        double cgpa = 3.5;
+        String department = "Computer Science";
+        
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Student Name: " + studentName);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Department: " + department);
+    }
+}
